da5c8d4484f477dc9a327961f2b3e80b8c28ac0f
Luiz Gabriel <luizgabriel.info@gmail.com>
2020-10-20 18:13:50 -0300

    Updated encoding

--- resources/views/prepare.blade.php
+++ resources/views/prepare.blade.php
@@ -53,10 +53,10 @@
                             <div class="form-group">
                                 <label for="encoding-input">Codificação:</label>
                                 <select id="encoding-input" name="encoding" class="form-control">
-                                    <option value="utf-8">UTF-8</option>
-                                    <option value="iso-8859-1">ISO-8859-1</option>
-                                    <option value="windows-1252" selected>WINDOWS-1252 (LATIN 1)</option>
-                                    <option value="ascii">ASCII</option>
+                                    <option value="UTF-8">UTF-8</option>
+                                    <option value="ISO-8859-1">ISO-8859-1</option>
+                                    <option value="Windows-1252" selected>WINDOWS-1252 (LATIN 1)</option>
+                                    <option value="ASCII">ASCII</option>
                                 </select>
                             </div>
 
@@ -70,7 +70,7 @@
 
                         </form>
 
-                        <div class="table-responsive">
+                        <div class="table-responsive mt-2">
                             <table class="table table-bordered" id="preview-table"></table>
                         </div>
 
