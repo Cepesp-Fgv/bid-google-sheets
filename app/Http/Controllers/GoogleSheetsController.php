88c78b28ca7bcbd56b7db27c88ed78c2e5a4b55a
Luiz Gabriel <luizgabriel.info@gmail.com>
2020-10-20 17:57:47 -0300

    Added utf8 convertion

--- app/Http/Controllers/GoogleSheetsController.php
+++ app/Http/Controllers/GoogleSheetsController.php
@@ -46,7 +46,7 @@ class GoogleSheetsController extends Controller
             $rowData = [];
             foreach ($row as $cell) {
                 if (filled($cell))
-                    array_push($rowData, mb_convert_encoding($cell, "UTF-8", "LATIN-1"));
+                    array_push($rowData, mb_convert_encoding($cell, "UTF-8", "Windows-1252"));
             }
 
             if (filled($rowData))
